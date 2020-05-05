01abe6105e1589d5a86070960a81b2594543e262
hongfu <1342735006@qq.com>
2020-05-05 09:41:07 +0800

    217.存在重复元素

---
+++ 217/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+namespace hongfu01 {
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> si;
+        for(auto it=nums.begin(); it!=nums.end(); it++) {
+            if(si.count(*it) > 0)
+                return true;
+            si.insert(*it);
+        }
+        return false;
+    }
+};
+}
+
+int main(int arg, char *argv[]) {
+
+    return0;
+}
